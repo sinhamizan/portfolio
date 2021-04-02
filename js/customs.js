0f2525b61eb4f3ff82421a6fd7904d0908b99f14
sinhamizan <mizan75kpi@gmail.com>
2021-04-02 22:28:24 +0600

    home page sructure completed.

---
+++ js/customs.js
@@ -0,0 +1,41 @@
+// mixitup
+var mixer = mixitup('.port-items');
+
+
+
+// scroll top
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+// Slider
+$( document ).ready(function() {
+    $('.technology.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:2
+            }
+        }
+    })
+});
